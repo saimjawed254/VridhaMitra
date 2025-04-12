c69bf2bb6fd212d643d821251e30887bd64c7d5d
Saim Jawed <saimjawed254@gmail.com>
2025-04-12 23:30:47 +0530

    default page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -25,7 +25,7 @@ createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<App />} />
+          <Route path='/' element={<User />} />
           <Route path='/login' element={<Login />} />
           <Route path='/sign-up' element={<Signup />} />
           <Route path='/test' element={<Test />} />
