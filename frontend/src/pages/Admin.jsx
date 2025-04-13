847f0976a85cfbe2004186d251fca3701053d3ab
Saim Jawed <saimjawed254@gmail.com>
2025-04-13 18:12:01 +0530

    assets tweak

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 function Admin() {
 
   const emailValue = useSelector((store) => store.email)
-  let usersArray=[]
+  let usersArray = []
   console.log(emailValue)
 
   // Sample data for the users table
@@ -19,54 +19,56 @@ function Admin() {
     { id: 5, status: "Do/MM/YYYY", recentCheck: 2 },
   ];
 
-  useEffect(()=>{
-    const fetchUsers=async()=>{
-      const usersData=await axios.post('https://vridhamitra.onrender.com/get-users',{
-        email:emailValue.email
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const usersData = await axios.post('https://vridhamitra.onrender.com/get-users', {
+        email: emailValue.email
       })
       console.log(usersData)
-      usersArray=usersData.usersArray;
+      usersArray = usersData.usersArray;
     }
     fetchUsers()
     console.log(usersArray)
-  },[])
+  }, [])
   return (
     <div className="app">
-      <Headerr/>
+      <Headerr />
       <main className="main-content">
         <div className="hero-section">
           <h2>BluePulse:A new age of healthcare is here</h2>
         </div>
-      <section className="section" id="section1">
-        <h3>Manage care for your loved ones</h3>
-        
-        <div className="table-container">
-          <table className="users-table">
-            <thead>
-              <tr>
-                <th>User</th>
-                <th>Status</th>
-                <th>Recent Check</th>
-              </tr>
-            </thead>
-            <tbody>
-              {users.map((user) => (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.status}</td>
-                  <td>{user.recentCheck}</td>
+        <section className="section" id="section1">
+          <h3>Manage care for your loved ones</h3>
+
+          <div className="table-container">
+            <table className="users-table">
+              <thead>
+                <tr>
+                  <th>User</th>
+                  <th>Status</th>
+                  <th>Recent Check</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-          <button className="view-all">View all</button>
-        </div>
-      </section>
-</main>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.status}</td>
+                    <td>{user.recentCheck}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <Link to='/add-user'>
+              <button className="view-all">Add User</button>
+            </Link>
+          </div>
+        </section>
+      </main>
       <section className="section">
         <h3>Recent Locations</h3>
         <div className="map-placeholder">
-        <img src="src\assets\map.jpg"/ >
+          <img src="src\assets\map.jpg" />
         </div>
         <div className="text"><p>Powered by Google Maps</p></div>
       </section>
