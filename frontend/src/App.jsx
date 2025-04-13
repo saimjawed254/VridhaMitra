32d074782af852334a4aa3994b2def783bbfe9ad
Saim Jawed <saimjawed254@gmail.com>
2025-04-13 15:47:13 +0530

    final final

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import './App.css';
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const App = () => {
 
@@ -36,7 +37,9 @@ const App = () => {
     </div>
     <div className="small-text">A single, intuitive app combining emergency response,<br /> daily health management using AI,
       and community support <br />designed specifically for seniors and their caregivers.</div>
+                <Link to='/user-home/health-form'>
       <div className="svg-text">Get Started</div>
+      </Link>
     <svg className="svg-get" xmlns="http://www.w3.org/2000/svg" width="200" height="200" viewBox="0 0 200 200" fill="none">
       <circle cx="84.5" cy="84.5" r="82" fill="url(#paint0_linear_4_29)" stroke="url(#paint1_linear_4_29)" stroke-width="5" />
       <defs>
